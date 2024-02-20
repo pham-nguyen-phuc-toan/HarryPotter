7c97bf2ca475b646f76cd75e956b7fe691292dfd
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2024-02-20 21:24:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,226 @@ import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 sorted(glob.glob('/harry-potter/*'))
+
+class CFG:
+    # LLMs
+    model_name = 'llama2-13b-chat' # wizardlm, llama2-7b-chat, llama2-13b-chat, mistral-7B
+    temperature = 0,
+    top_p = 0.95,
+    repetition_penalty = 1.15    
+
+    # splitting
+    split_chunk_size = 800
+    split_overlap = 0
+    
+    # embeddings
+    embeddings_model_repo = 'sentence-transformers/all-MiniLM-L6-v2'    
+
+    # similar passages
+    k = 3
+    
+    # paths
+    PDFs_path = '/kaggle/input/harry-potter-books-in-pdf-1-7/HP books/'
+    Embeddings_path =  '/kaggle/input/faiss-hp-sentence-transformers'
+    Persist_directory = './harry-potter-vectordb'
+
+def get_model(model = CFG.model_name):
+
+    print('\nDownloading model: ', model, '\n\n')
+
+    if model == 'wizardlm':
+        model_repo = 'TheBloke/wizardLM-7B-HF'
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_repo)
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_repo,
+            load_in_4bit = True,
+            device_map = 'auto',
+            torch_dtype = torch.float16,
+            low_cpu_mem_usage = True
+        )
+        
+        max_len = 1024
+
+    elif model == 'llama2-7b-chat':
+        model_repo = 'daryl149/llama-2-7b-chat-hf'
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_repo, use_fast=True)
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_repo,
+            load_in_4bit = True,
+            device_map = 'auto',
+            torch_dtype = torch.float16,
+            low_cpu_mem_usage = True,
+            trust_remote_code = True
+        )
+        
+        max_len = 2048
+
+    elif model == 'llama2-13b-chat':
+        model_repo = 'daryl149/llama-2-13b-chat-hf'
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_repo, use_fast=True)
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_repo,
+            load_in_4bit = True,        
+            device_map = 'auto',
+            torch_dtype = torch.float16,
+            low_cpu_mem_usage = True,
+            trust_remote_code = True
+        )
+        
+        max_len = 2048 # 8192
+
+    elif model == 'mistral-7B':
+        model_repo = 'mistralai/Mistral-7B-v0.1'
+        
+        tokenizer = AutoTokenizer.from_pretrained(model_repo)
+
+        model = AutoModelForCausalLM.from_pretrained(
+            model_repo,
+            load_in_4bit = True,
+            device_map = 'auto',
+            torch_dtype = torch.float16,
+            low_cpu_mem_usage = True,
+        )
+        
+        max_len = 1024
+
+    else:
+        print("Not implemented model (tokenizer and backbone)")
+
+    return tokenizer, model, max_len
+
+tokenizer, model, max_len = get_model(model = CFG.model_name)
+
+pipe = pipeline(
+    task = "text-generation",
+    model = model,
+    tokenizer = tokenizer,
+    pad_token_id = tokenizer.eos_token_id,
+    max_length = max_len,
+    temperature = CFG.temperature,
+    top_p = CFG.top_p,
+    repetition_penalty = CFG.repetition_penalty
+)
+
+llm = HuggingFacePipeline(pipeline = pipe)
+
+loader = DirectoryLoader(
+    CFG.PDFs_path,
+    glob="./*.pdf",
+    loader_cls=PyPDFLoader,
+    show_progress=True,
+    use_multithreading=True
+)
+
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = CFG.split_chunk_size,
+    chunk_overlap = CFG.split_overlap
+)
+
+texts = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceInstructEmbeddings(
+    model_name = CFG.embeddings_model_repo,
+    model_kwargs = {"device": "cuda"}
+)
+
+### create embeddings and DB
+vectordb = FAISS.from_documents(
+    documents = texts, 
+    embedding = embeddings
+)
+
+### persist vector database
+vectordb.save_local("faiss_index_hp")
+
+
+### download embeddings model
+embeddings = HuggingFaceInstructEmbeddings(
+    model_name = CFG.embeddings_model_repo,
+    model_kwargs = {"device": "cuda"}
+)
+
+### load vector DB embeddings
+vectordb = FAISS.load_local(
+    CFG.Embeddings_path,
+    embeddings
+)
+
+prompt_template = """
+Don't try to make up an answer, if you don't know just say that you don't know.
+Answer in the same language the question was asked.
+Use only the following pieces of context to answer the question at the end.
+
+{context}
+
+Question: {question}
+Answer:"""
+
+
+PROMPT = PromptTemplate(
+    template = prompt_template, 
+    input_variables = ["context", "question"]
+)
+
+llm_chain = LLMChain(prompt=PROMPT, llm=llm)
+
+retriever = vectordb.as_retriever(search_kwargs = {"k": CFG.k, "search_type" : "similarity"})
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm = llm,
+    chain_type = "stuff", # map_reduce, map_rerank, stuff, refine
+    retriever = retriever, 
+    chain_type_kwargs = {"prompt": PROMPT},
+    return_source_documents = True,
+    verbose = False
+)
+
+def wrap_text_preserve_newlines(text, width=700):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+
+    return wrapped_text
+
+def process_llm_response(llm_response):
+    ans = wrap_text_preserve_newlines(llm_response['result'])
+    
+    sources_used = ' \n'.join(
+        [
+            source.metadata['source'].split('/')[-1][:-4] + ' - page: ' + str(source.metadata['page'])
+            for source in llm_response['source_documents']
+        ]
+    )
+    
+    ans = ans + '\n\nSources: \n' + sources_used
+    return ans
+
+def llm_ans(query):
+    start = time.time()
+    
+    llm_response = qa_chain.invoke(query)
+    ans = process_llm_response(llm_response)
+    
+    end = time.time()
+
+    time_elapsed = int(round(end - start, 0))
+    time_elapsed_str = f'\n\nTime elapsed: {time_elapsed} s'
+    return ans + time_elapsed_str
+
+import streamlit as st
+
+query = st.text_input('Insert a query', '')
+st.write(llm_ans(query))
