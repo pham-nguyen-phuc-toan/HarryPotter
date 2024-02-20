7496344a8128b5c8a9055d5aacff4ac495bf5366
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2024-02-20 21:09:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import warnings
+warnings.filterwarnings("ignore")
 
+import os
+import glob
+import textwrap
+import time
+
+import langchain
+
+# loaders
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+
+# splits
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# prompts
+from langchain import PromptTemplate, LLMChain
+
+# vector stores
+from langchain.vectorstores import FAISS
+
+# models
+from langchain.llms import HuggingFacePipeline
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+# retrievers
+from langchain.chains import RetrievalQA
+
+import torch
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+sorted(glob.glob('/harry-potter/*'))
