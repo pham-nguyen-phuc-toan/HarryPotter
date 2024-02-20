b9707c0c426c55a487b8ad6d74e1e2dbfff3a5b0
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2024-02-20 19:09:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ openai.api_key = 'YOU-API-KEY'
 print("started the loading document process...")
 
 # Read the data from the specified directory. Change './boiler_docs/' to your desired path.
-documents = SimpleDirectoryReader('/kaggle/input/nlp-and-llm-related-arxiv-papers/').load_data()
+documents = SimpleDirectoryReader('/arxiv-paper/').load_data()
 
 # Initialize the LLMPredictor with the desired GPT-3.5-turbo model and temperature setting
 llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"))
